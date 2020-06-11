fbd1ec6266a2ba17a062322265367fa2a9241ba4
BardoftheOzarks <1886@holbertonschool.com>
2020-06-11 22:35:36 +0000

    Added task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ *
+ * Return: prints alphabet in lower case
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
