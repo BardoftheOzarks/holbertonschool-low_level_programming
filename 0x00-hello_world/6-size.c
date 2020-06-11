c96a6d6fbaa76c21c567d5167156f592b34de014
BardoftheOzarks <1886@holbertonschool.com>
2020-06-11 01:13:06 +0000

    added task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+/**
+ *main - prints sizes of types
+ *
+ *Return: Give the sizes of basic data types
+ */
+#include <stdio.h>
+#include <limits.h>
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char:%d \n", sizeof(a));
+	printf("Size of an int:%d \n", sizeof(b));
+	printf("Size of a long int:%d \n", sizeof(c));
+	printf("Size of a long long int:%d \n", sizeof(d));
+	printf("Size of a float:%d \n", sizeof(e));
+	return (0);
+}
