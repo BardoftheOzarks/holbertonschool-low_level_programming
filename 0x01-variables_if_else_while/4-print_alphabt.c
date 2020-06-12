19070242c5822ecb51b44e0d0b05beb498a83038
BardoftheOzarks <1886@holbertonschool.com>
2020-06-12 00:17:46 +0000

    Added task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints most alphabet
+ *
+ * Return: prints alphabet in lower case
+ * except q and e
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch != "e", ch != "q", ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
