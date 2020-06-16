7e33734c3e8fbf2360b76af46c09a293214bc970
BardoftheOzarks <1886@holbertonschool.com>
2020-06-16 18:38:12 +0000

    Added tasks 3 & 4

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _islower - determines if char is lower case
+ */
+
+int _islower(int c)
+{
+	if (c >= 97)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - determines if char is alpha
+ *
+ * Return: 1 if alpha
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
