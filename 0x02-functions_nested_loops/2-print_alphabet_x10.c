981577437b57fbe7dd49dc15f155e5b893d850f4
BardoftheOzarks <1886@holbertonschool.com>
2020-06-16 16:29:47 +0000

    Added task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10x
+ */
+
+void print_alphabet_x10(void)
+{
+	int c;
+	int i;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
