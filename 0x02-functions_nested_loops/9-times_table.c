8c1d16d559b6070eb0d6fce49df83c4cc15b8b61
BardoftheOzarks <1886@holbertonschool.com>
2020-06-17 01:29:12 +0000

    hotfix task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,54 +1,39 @@
 #include "holberton.h"
-
 /**
  * times_table - prints a table
  *
  * Return: times table 0-9
  */
-
 void times_table(void)
-{
-	int value, count, mult, tens, ones;
+{	int value, count, mult, tens, ones;
 
 	for (mult = 0 ; mult <= 9 ; mult++)
-	{
-		value = 0;
+	{	value = 0;
 		_putchar(value + '0');
 		for (count = 1 ; count <= 9 ; count++)
-		{
-			value = value + mult;
+		{	value = value + mult;
 			tens = value / 10;
 			ones = value % 10;
 			if (count < 9 && value <= 9)
-			{
-				_putchar(',');
+			{	_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(ones + '0');
-			}
-			else if (count < 9 && value > 9)
-			{
-				_putchar(',');
+			} else if (count < 9 && value > 9)
+			{	_putchar(',');
 				_putchar(' ');
 				_putchar(tens + '0');
 				_putchar(ones + '0');
-			}
-			else if (count == 9 && value <= 9)
-			{
-				_putchar(',');
-                                _putchar(' ');
+			} else if (count == 9 && value <= 9)
+			{	_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 				_putchar(ones + '0');
 				_putchar('\n');
-			}
-			else if (count == 9 && value > 9)
-			{
-				_putchar(',');
-                                _putchar(' ');
-                                _putchar(tens + '0');
-                                _putchar(ones + '0');
+			} else if (count == 9 && value > 9)
+			{	_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
 				_putchar('\n');
-			}
-		}
-	}
-}
+			}}}}
