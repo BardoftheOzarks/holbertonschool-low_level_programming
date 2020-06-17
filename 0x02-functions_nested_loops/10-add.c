28200ed962531e683c3d90e25370ad00c8e27621
BardoftheOzarks <1886@holbertonschool.com>
2020-06-17 02:12:55 +0000

    Added tasks 10 & 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - adds two variables together
+ *
+ * Return: int1 + int2
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints int to 98
+ *
+ * Return: prints a sequence of numbers to 98
+ */
+void print_to_98(int n)
+{
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+		printf("98\n");
+}
