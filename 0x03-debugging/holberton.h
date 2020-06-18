1abfea19424860db1c11ca65ce7613a0111c7b2b
BardoftheOzarks <1886@holbertonschool.com>
2020-06-18 23:34:26 +0000

    Added task 3

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -7,5 +7,7 @@ int _putchar(char c);
 void jack_bauer(void);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif /* HOLBERTON_H */
