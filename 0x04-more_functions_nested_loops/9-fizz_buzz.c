b926bdf6f425fa3a778cb0018deb632217b14208
BardoftheOzarks <1886@holbertonschool.com>
2020-06-18 21:48:48 +0000

    hotfix task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,13 @@ int main(void)
 	for (i = 1 ; i <= 100 ; i++)
 	{
 		if (i % 3 == 0)
-		{
 			printf("Fizz");
-		}
 		if (i % 5 == 0)
-		{
 			printf("Buzz");
-		}
 		if (i % 3 != 0 && i % 5 != 0)
-		{
 			printf("%d", i);
-		}
+		if (i == 100)
+			break;
 		printf(" ");
 	}
 	printf("\n");
