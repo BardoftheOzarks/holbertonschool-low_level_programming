7d32c473e5b021530d68a342d82ee5337fe4952e
BardoftheOzarks <1886@holbertonschool.com>
2020-06-18 19:32:56 +0000

    hotfix 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,15 +7,21 @@
  * Description: prints _ x int n
  */
 
-void print_line(int n)
+void print_diagonal(int n)
 {
-	for (; n > 1; n--)
+	int h, l;
+
+	for (h = n ; h > 0 ; h--)
 	{
-		_putchar(' ');
-	}
-	if (n > 0)
-	{
-		_putchar('\\');
-	}
+		for (l = n - h ; l > 0 ; l--)
+		{
+			_putchar(' ');
+		}
+	_putchar('\\');
 	_putchar('\n');
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
 }
