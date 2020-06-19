4ef5cc21e4946d56aebb8026808eb717e3593df5
BardoftheOzarks <1886@holbertonschool.com>
2020-06-19 00:04:14 +0000

    hotfix task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,12 +14,16 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0)
 	{
+
 		if (month >= 2 && day >= 60)
 		{
-			day++;
+			printf("Day of the year: %d\n", day);
+			day--;
 		}
-
-		printf("Day of the year: %d\n", day);
+		else
+		{
+			printf("Day of the year: %d\n", day);
+		}
 		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
