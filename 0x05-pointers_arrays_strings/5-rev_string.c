870e0312997b16234e26d054e9a5eba3c0a647d6
BardoftheOzarks <1886@holbertonschool.com>
2020-06-24 03:44:20 +0000

    Hotfix task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,6 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <string.h>
 /**
  * rev_string - reverses a string
  * @s: an string
@@ -7,17 +9,29 @@
  */
 void rev_string(char *s)
 {
-	int count, begin, end = 0;
-	char r[];
+	int c, length = 0;
+	char *begin, *end, temp;
 
-	while (s[count] != '\0')
+	begin = end = s;
+	length = _strlen(s);
+	for (c = 0; c < length - 1; c++)
+		end++;
+ 	for (c = 0; c < length / 2; c++)
 	{
-		count++;
-	}
-	end = count - 1;
-	for (; begin < count ; begin++)
-	{
-		r[begin] = s[end];
+		temp = *end;
+		*end = *begin;
+		*begin = temp;
+		begin++;
 		end--;
 	}
+}
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	return (c);
 }
