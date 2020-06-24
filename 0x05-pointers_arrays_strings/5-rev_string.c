d2313c3bf86f170a5de73c12793de8bd65265d2e
BardoftheOzarks <1886@holbertonschool.com>
2020-06-24 03:49:31 +0000

    betty formatted

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,8 @@
 #include "holberton.h"
-#include <stdio.h>
-#include <string.h>
+
 /**
  * rev_string - reverses a string
  * @s: an string
- *
- * Return: reverse string
  */
 void rev_string(char *s)
 {
@@ -16,7 +13,7 @@ void rev_string(char *s)
 	length = _strlen(s);
 	for (c = 0; c < length - 1; c++)
 		end++;
- 	for (c = 0; c < length / 2; c++)
+	for (c = 0; c < length / 2; c++)
 	{
 		temp = *end;
 		*end = *begin;
@@ -25,6 +22,12 @@ void rev_string(char *s)
 		end--;
 	}
 }
+/**
+ * _strlen - counts string length
+ * @s: an string
+ *
+ * Return: string length
+ */
 int _strlen(char *s)
 {
 	int c = 0;
