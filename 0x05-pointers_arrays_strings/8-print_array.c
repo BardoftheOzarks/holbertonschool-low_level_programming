64728821c4917997f4fb34bb9743bacd05506cd6
BardoftheOzarks <1886@holbertonschool.com>
2020-06-25 16:48:36 +0000

    Added task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,40 +1,19 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
- * rev_string - reverses a string
- * @s: an string
+ * print_array - prints an array
+ * @a: a pointer
+ * @n: length of a string
  */
-void rev_string(char *s)
-{
-	int c, length = 0;
-	char *begin, *end, temp;
-
-	begin = end = s;
-	length = _strlen(s);
-	for (c = 0; c < length - 1; c++)
-		end++;
-	for (c = 0; c < length / 2; c++)
-	{
-		temp = *end;
-		*end = *begin;
-		*begin = temp;
-		begin++;
-		end--;
-	}
-}
-/**
- * _strlen - counts string length
- * @s: an string
- *
- * Return: string length
- */
-int _strlen(char *s)
+void print_array(int *a, int n)
 {
 	int c = 0;
 
-	while (s[c] != '\0')
+	while(c < n - 1)
 	{
+		printf("%d, ", *a);
+		a++;
 		c++;
 	}
-	return (c);
+	printf("%d\n", *a);
 }
