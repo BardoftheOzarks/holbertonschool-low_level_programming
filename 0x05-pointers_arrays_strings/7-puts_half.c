eb262f838c07d1d868a013c5b849216ae2eda292
BardoftheOzarks <1886@holbertonschool.com>
2020-06-25 00:56:08 +0000

    hotfix task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * puts_half - prints half a string
  * @str: an string
@@ -7,15 +6,15 @@
 void puts_half(char *str)
 {
 	int c, length = 0;
-	char *begin, *end, temp;
 
 	while (str[c] != '\0')
 		c++;
-	length = c - 1;
+	length = c;
 	if (length % 2 == 0)
 		c = length / 2;
 	else
 		c = (length - 1) / 2;
-	for(; c < length; c++)
-		_putchar(str[c];
+	for (; c < length; c++)
+		_putchar(str[c]);
+	_putchar('\n');
 }
