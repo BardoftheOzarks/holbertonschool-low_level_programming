82d51e3ce621093de40c475a0dc8771ad8824fd5
BardoftheOzarks <1886@holbertonschool.com>
2020-06-26 00:19:52 +0000

    bettied task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,10 @@
 #include "holberton.h"
 /**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
  *
+ * Return: dest string
  */
 char *_strcat(char *dest, char *src)
 {
