c75074ddbbee85d4cefcac7581478fb986f1c777
BardoftheOzarks <1886@holbertonschool.com>
2020-06-29 21:33:36 +0000

    betty formated

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,10 +4,11 @@
  * @s: a pointer
  * @b: a constant byte
  * @n: number of bytes
+ * Return: s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	while (n-- > 0)
 		*s++ = b;
-	return s;
+	return (s);
 }
