f8159822e67cb06d129f9b823488496994a9962f
BardoftheOzarks <1886@holbertonschool.com>
2020-07-01 23:06:21 +0000

    added tasks 2 & 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * factorial - returns factorial of a given number
+ * @n: an int
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n <= 1)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
