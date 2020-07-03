08d43af25bf71aa8dd6e799a01a61564419ac6a8
BardoftheOzarks <1886@holbertonschool.com>
2020-07-03 04:33:17 +0000

    added task 7

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -19,10 +19,10 @@ int is_palindrome(char *s)
  */
 int _strlen_recursion(char *s)
 {
-        if (*s == '\0')
-                return (0);
-        else
-                return (1 + _strlen_recursion(s + 1));
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
 /**
  * find_palindrome - finds palindrome
