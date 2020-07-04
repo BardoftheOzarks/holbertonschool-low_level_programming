06528609b9322a56b0b6857ef1b27f219b4aabba
BardoftheOzarks <1886@holbertonschool.com>
2020-07-04 04:28:56 +0000

    added task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints Fibonacci sequence
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int c, d = 3;
+
+	printf("%d, ", a);
+	printf("%d, ", b);
+	printf("%d, ", c);
+	for (d = 4; d <= 49; d++)
+	{
+		a = b;
+		b = c;
+		c = a + b;
+		printf("%d, ", c);
+	}
+	a = b;
+	b = c;
+	c = a + b;
+	printf("%d", c);
+	return (0);
+}
