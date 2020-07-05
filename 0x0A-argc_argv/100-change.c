6c6bcb20a76c67b6a9128602358cac4b2f0c2ef3
BardoftheOzarks <1886@holbertonschool.com>
2020-07-05 03:55:21 +0000

    betty formatted

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,24 +14,24 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 	{	printf("Error\n");
-		return (1);}
-	while (sum > 0){
-		if (sum >= 25){
-			change = change + (sum / 25);
-			sum = sum % 25;}
-		if (sum >= 10){
-			change = change + (sum / 10);
-			sum = sum % 10;}
-		if (sum >= 5){
-			change = change + (sum / 5);
-			sum = sum % 5;}
-		if (sum >= 2){
-			change = change + (sum / 2);
-			sum = sum % 2;}
-		if (sum == 1){
-			change = change + 1;
-			sum = sum - 1;}
-	}
+		return (1); }
+	while (sum > 0)
+	{
+		if (sum >= 25)
+		{	change = change + (sum / 25);
+			sum = sum % 25;	}
+		if (sum >= 10)
+		{	change = change + (sum / 10);
+			sum = sum % 10;	}
+		if (sum >= 5)
+		{	change = change + (sum / 5);
+			sum = sum % 5;	}
+		if (sum >= 2)
+		{	change = change + (sum / 2);
+			sum = sum % 2;	}
+		if (sum == 1)
+		{	change = change + 1;
+			sum = sum - 1;	}}
 	printf("%d\n", change);
 	return (0);
 }
