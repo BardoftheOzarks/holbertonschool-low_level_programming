9ac4ae9d5f37533138fcd949bf75aa6560e58545
BardoftheOzarks <1886@holbertonschool.com>
2020-07-08 03:37:48 +0000

    added task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - makes an array of ints
+ * @width: width
+ * @height: height
+ * Return: pointer to new array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int wide, tall;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	array = (int **)malloc(sizeof(int *) * height);
+	for (tall = 0; tall < height; tall++)
+	{
+		array[tall] = (int *)malloc(sizeof(int) * width);
+		for (wide = 0; wide < width; wide++)
+			array[tall][wide] = 0;
+	}
+	return (array);
+}
