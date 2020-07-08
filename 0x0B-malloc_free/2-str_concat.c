2db5ffde809427a2a6fb15e7bec11775328c977d
BardoftheOzarks <1886@holbertonschool.com>
2020-07-08 05:50:17 +0000

    fixed #2?

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,17 +16,19 @@ char *str_concat(char *s1, char *s2)
 	dup = malloc(sizeof(s1) + sizeof(s2));
 	if (dup == NULL)
 		return (NULL);
-	while (s1[cnt1] != '\0')
-	{
-		dup[cnt1] = s1[cnt1];
-		cnt1++;
-	}
-	while (s2[cnt2] != '\0')
-	{
-		dup[cnt1] = s2[cnt2];
-		cnt1++;
-		cnt2++;
-	}
+	if (s1 != NULL)
+		while (s1[cnt1] != '\0')
+		{
+			dup[cnt1] = s1[cnt1];
+			cnt1++;
+		}
+	if (s2 != NULL)
+		while (s2[cnt2] != '\0')
+		{
+			dup[cnt1] = s2[cnt2];
+			cnt1++;
+			cnt2++;
+		}
 	dup[cnt1] = '\0';
 	return (dup);
 }
