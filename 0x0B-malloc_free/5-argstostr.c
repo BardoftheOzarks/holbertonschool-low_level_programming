f2c49426985203e31caf580b937619435d37a9d1
BardoftheOzarks <1886@holbertonschool.com>
2020-07-08 04:55:04 +0000

    slot fill

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all arg
+ * @ac: argc
+ * @av: argv
+ */
+
+char *argstostr(int ac, char **av)
+{
+	ac--;
+	return (av);
+}
