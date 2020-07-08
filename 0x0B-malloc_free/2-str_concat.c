807ceddbb8871915ea736af27d61805c2b12e33e
BardoftheOzarks <1886@holbertonschool.com>
2020-07-08 21:40:36 +0000

    hotfix task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,21 +13,23 @@ char *str_concat(char *s1, char *s2)
 	char *dup;
 	int cnt1 = 0, cnt2 = 0;
 
-	dup = malloc(sizeof(s1) + sizeof(s2));
+	if (s1 != NULL)
+		while (s1[cnt1] != '\0')
+			cnt1++;
+	if (s2 != NULL)
+		while (s2[cnt2] != '\0')
+			cnt2++;
+	dup = malloc((sizeof(char) * cnt1) + (sizeof(char) * cnt2) + 1);
 	if (dup == NULL)
 		return (NULL);
 	if (s1 != NULL)
-		while (s1[cnt1] != '\0')
-		{
+		for (cnt1 = 0; s1[cnt1] != '\0'; cnt1++)
 			dup[cnt1] = s1[cnt1];
-			cnt1++;
-		}
 	if (s2 != NULL)
-		while (s2[cnt2] != '\0')
+		for (cnt2 = 0; s2[cnt2] != '\0'; cnt2++)
 		{
 			dup[cnt1] = s2[cnt2];
 			cnt1++;
-			cnt2++;
 		}
 	dup[cnt1] = '\0';
 	return (dup);
