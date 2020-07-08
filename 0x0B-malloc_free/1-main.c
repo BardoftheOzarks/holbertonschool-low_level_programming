d41f150109f9b9f2f070a2dc8e84c1d36ab72901
BardoftheOzarks <1886@holbertonschool.com>
2020-07-08 05:43:47 +0000

    hotfix 2

--- 0x0B-malloc_free/1-main.c
+++ 0x0B-malloc_free/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char *s;
 
-	s = _strdup("null");
+	s = _strdup("And then there were fewer");
 	if (s == NULL)
 	{
 		printf("failed to allocate memory\n");
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,6 @@ char *str_concat(char *s1, char *s2)
 	char *dup;
 	int cnt1 = 0, cnt2 = 0;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
 	dup = malloc(sizeof(s1) + sizeof(s2));
 	if (dup == NULL)
 		return (NULL);
