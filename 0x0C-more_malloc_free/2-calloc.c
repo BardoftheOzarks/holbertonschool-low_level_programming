c9cdc3eb2457d82433fe5f4b6d2e0051c04e6046
BardoftheOzarks <1886@holbertonschool.com>
2020-07-09 18:24:50 +0000

    added task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - makes an array of ints
+ * @nmemb: width
+ * @size: height
+ * Return: pointer to new array
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *array;
+	unsigned int cnt;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	array = malloc(nmemb * size);
+	if (array == NULL)
+		return (NULL);
+	for (cnt = 0; cnt < nmemb; cnt++)
+			array[cnt] = 0;
+	array[cnt] = '\0';
+	return ((void *)array);
+}
