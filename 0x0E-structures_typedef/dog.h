227ff3661b9ca8ceee92e96154a1ff5dbc1ac946
BardoftheOzarks <1886@holbertonschool.com>
2020-07-13 22:13:12 +0000

    added task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+
 /**
  * struct dog - doggie data
  * @name: dog's name
@@ -12,4 +13,6 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
