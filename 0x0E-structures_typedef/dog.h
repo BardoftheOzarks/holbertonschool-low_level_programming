fb272f6b276c8f8292d2afac38ee5a1da72ac582
BardoftheOzarks <1886@holbertonschool.com>
2020-07-13 20:28:35 +0000

    initial push

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - doggie data
+ * @name: dog's name
+ * @age: age in 0.0 format
+ * @owner: puppy parents
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
