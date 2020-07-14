a7da2593e18d1d8aa578fdead275dacc0e8a161a
BardoftheOzarks <1886@holbertonschool.com>
2020-07-14 04:17:46 +0000

    hotfix task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,16 +6,15 @@
  */
 void print_dog(struct dog *d)
 {
-        int cnt = 0;
         if (d == NULL)
                 return;
         if (d->name == NULL)
                 printf("Name: (nil)\n");
         else
-                printf("Name: %s\n", (*d).name);
-	printf("Age: %.6f\n", (*d).age);
+                printf("Name: %s\n", d->name);
+	printf("Age: %.6f\n", d->age);
         if ((*d).owner == NULL)
 		printf("Owner: (nil)\n");
 	else
-		printf("Owner: %s\n");
+		printf("Owner: %s\n", d->owner);
 }
