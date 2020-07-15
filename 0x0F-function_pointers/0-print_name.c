8397424b5cb07c797ea076e03907b04c75c06d47
BardoftheOzarks <1886@holbertonschool.com>
2020-07-15 18:57:00 +0000

    removed prototype from header

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <strings.h>
+
 /**
  * print_name - doesn't actually print anything
  * @name: string for the name
@@ -7,7 +7,5 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
 	(*f)(name);
 }
