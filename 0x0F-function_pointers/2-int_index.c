ac84ddc019700178fc7a4950c73737eeb798705b
BardoftheOzarks <1886@holbertonschool.com>
2020-07-15 20:34:15 +0000

    added task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - performs an action on each member of an array
+ * @array: input array
+ * @size: number of cells in array
+ * @cmp: function that compares values
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (i = 0; (*cmp)(array[i]) == 0 && i < size; i++);
+	if (i == size)
+		return (-1);
+	return (i);
+}
