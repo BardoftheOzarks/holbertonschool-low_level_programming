ef43654141280615098706c2d3d8da64f3c0aa37
BardoftheOzarks <1886@holbertonschool.com>
2020-07-15 20:40:13 +0000

    bettied task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,6 +5,7 @@
  * @array: input array
  * @size: number of cells in array
  * @cmp: function that compares values
+ * Return: position of first match
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -12,7 +13,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
-	for (i = 0; (*cmp)(array[i]) == 0 && i < size; i++);
+	for (i = 0; (*cmp)(array[i]) == 0 && i < size; i++)
+		;
 	if (i == size)
 		return (-1);
 	return (i);
