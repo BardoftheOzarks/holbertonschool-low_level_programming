641147b81289e2e51e7e10a0a75d685b80f11b55
BardoftheOzarks <1886@holbertonschool.com>
2020-07-19 17:50:46 +0000

    changed n cancellation

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,7 +12,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	if (n == 0)
+	{
+		printf("\n");
 		return;
+	}
 	va_start(list, n);
 	for (i = 0; i < n - 1; i++)
 	{
