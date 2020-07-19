21c928db1613e9e08dd12642e711055aece092db
BardoftheOzarks <1886@holbertonschool.com>
2020-07-19 19:52:43 +0000

    adjusted bytes

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,11 +1,11 @@
-          global    _start
+	global    _start
 
-	          section   .text
+	section   .text
 _start:
 	mov	rax, 1		; system call for write
 	mov	rdi, 1		; file handle 1 is stdout
 	mov	rsi, message 	; address of string to output
-	mov	rdx, 13		; number of bytes
+	mov	rdx, 17		; number of bytes
 	syscall			; invoke operating system to do the write
 	mov	rax, 60		; system call for exit
 	xor	rdi, rdi	; exit code 0
