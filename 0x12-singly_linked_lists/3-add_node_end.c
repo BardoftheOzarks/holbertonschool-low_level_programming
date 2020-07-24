05378edf473acd3c8b8ca7914d430a617aa20a63
BardoftheOzarks <1886@holbertonschool.com>
2020-07-24 04:25:23 +0000

    adjusted NULL str factor

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -23,8 +23,6 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new, *scan;
 
-	if (str == NULL)
-		return (NULL);
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
