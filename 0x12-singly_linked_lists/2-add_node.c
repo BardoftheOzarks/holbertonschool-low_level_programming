b67bcf9cca35870acc89da8ccdfef24a2388700d
BardoftheOzarks <1886@holbertonschool.com>
2020-07-24 02:16:34 +0000

    added task 6

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - returns size of list_t
+ * @h: pointer to list_t
+ * Return: size_t
+ */
+size_t list_len(const list_t *h)
+{
+	int nodes = 0;
+
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
