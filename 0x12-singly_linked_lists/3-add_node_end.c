65f5ed148f50c7faacd04c8574f2108cc315fffa
BardoftheOzarks <1886@holbertonschool.com>
2020-07-24 04:03:20 +0000

    added task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+/**
+ * _strlen - counts string length
+ * @s: an string
+ *
+ * Return: string length
+ */
+int _strlen(const char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
+/**
+ * add_node_end - adds a list_t to end
+ * @head: pointer to list_t
+ * @str: input string
+ * Return: * to new list
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *scan;
+
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
+		return (NULL);
+	new->len = _strlen(str);
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		scan = *head;
+		while (scan->next != NULL)
+			scan = scan->next;
+		scan->next = new;
+	}
+	return (new);
+}
