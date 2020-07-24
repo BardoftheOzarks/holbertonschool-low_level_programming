fb7de60b8fb938b214ab0a63d717289cc703d6c5
BardoftheOzarks <1886@holbertonschool.com>
2020-07-24 04:43:18 +0000

    added task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+/**
+ * ur_beat - beat ya to it
+ */
+void __attribute__((constructor))ur_beat(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
