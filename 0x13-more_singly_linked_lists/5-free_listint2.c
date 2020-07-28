bc25a2cc3f57c595fa04d0ed91aeef5908107160
BardoftheOzarks <1886@holbertonschool.com>
2020-07-28 15:22:03 +0000

    added task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t
+ * @head: pointer to listint_t
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *scan = head, *tmp;
+
+	while (scan)
+	{
+		tmp = scan;
+		scan = scan->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
