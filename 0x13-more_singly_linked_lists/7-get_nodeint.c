73692274c376ddf52f92318b8b9fcc3225e25f9a
BardoftheOzarks <1886@holbertonschool.com>
2020-07-28 20:04:52 +0000

    added task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * get_nodeint - finds a node
+ * @head: head of the list
+ * Return: int from head
+ */
+listint_t *get_nodeint(listint_t **head, unsigned int index)
+{
+	listint_t *scan = *head;
+	int n = 0;
+
+	while (n <= index)
+	{
+		if (scan->next == NULL)
+			return (NULL);
+		scan = scan->next;
+		n++;
+	}
+	return (scan);
+}
