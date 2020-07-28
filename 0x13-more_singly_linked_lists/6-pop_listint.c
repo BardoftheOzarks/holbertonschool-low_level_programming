7191a523cb4a76e81125eee3a874fa8fd61d236e
BardoftheOzarks <1886@holbertonschool.com>
2020-07-28 17:29:42 +0000

    bettied

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,12 +1,14 @@
 #include "lists.h"
 /**
  * pop_listint - frees a listint_t head
- * @head: int from head
+ * @head: head of the list
+ * Return: int from head
  */
 int pop_listint(listint_t **head)
 {
 	listint_t *scan = *head, *tmp;
 	int n;
+
 	if (head == NULL)
 		return (0);
 	n = scan->n;
