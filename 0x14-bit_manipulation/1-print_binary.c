94a2f17ec01a916e5bb2be29874afd3c00573d54
BardoftheOzarks <1886@holbertonschool.com>
2020-07-31 02:25:09 +0000

    added task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,9 +5,24 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int digit;
+	unsigned int digit = 1;
 
-	while (n > 0)
+	while (digit <= n)
+		digit = digit << 1;
+	digit = digit >> 1;
+	while (digit > 0)
 	{
-		for (digit = 1; 
+		if (n >= digit)
+		{
+			_putchar('1');
+			n -= digit;
+			digit = digit >> 1;
+		}
+		else
+		{
+			_putchar('0');
+			digit = digit >> 1;
+		}
+	}
+	_putchar('\n');
 }
