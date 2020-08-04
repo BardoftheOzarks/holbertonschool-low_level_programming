1d5d4f03e9e33087a0ea12829c3367b23f925941
BardoftheOzarks <1886@holbertonschool.com>
2020-08-04 19:51:03 +0000

    added task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * read_textfile - reads text file and prints to std_out
+ * @filename: input file name
+ * @letters: number of char to read/print
+ * Return: actual number of letters read/printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, rd, wr;
+	char *c;
+
+	fd = open(filename, O_RDONLY, 0400);
+	c = malloc(sizeof(char) * letters);
+	if (fd == -1 || filename == NULL || c == NULL)
+		return (0);
+	rd = read(fd, c, letters);
+	wr = write(STDOUT_FILENO, c, rd);
+	close(fd);
+	free(c);
+	return (wr);
+}
