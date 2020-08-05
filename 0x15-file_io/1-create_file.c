dc51e59d74bb886d95a7ba6853f639772b24f79f
BardoftheOzarks <1886@holbertonschool.com>
2020-08-05 02:44:10 +0000

    added edge case clause

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,11 +11,16 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_TRUNC | O_CREAT | O_WRONLY, 0600);
 	if (fd == -1)
 	{
 		close(fd);
 		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
 	}
 	while (text_content[size])
 		size++;
