1a250478d0f56fd7af04556947e332c1486bbce0
BardoftheOzarks <1886@holbertonschool.com>
2020-08-05 01:55:30 +0000

    switched to creat from open

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,8 +11,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = creat(filename, 0600);
 	if (fd == -1)
+		close(fd);
 		return (-1);
 	while (text_content[size] != '\0')
 		size++;
