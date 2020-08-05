42cdd5cdc3cb7701414548a584165351dcea39e1
BardoftheOzarks <1886@holbertonschool.com>
2020-08-05 00:52:20 +0000

    testing macro removal

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - cp a file to another file
+ * @av: argument vector
+ * @ac: argument count
+ * Return:
+ * 97: incorrect arg count
+ * 98: invalid src
+ * 99: write fail
+ * 100: close fail
+ */
+int main(ac, **av)
+{
+	
+}
