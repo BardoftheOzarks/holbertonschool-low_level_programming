cbd2e62a810333993f70d7b628f4b116f10cac95
BardoftheOzarks <1886@holbertonschool.com>
2020-10-02 06:02:12 +0000

    bettied

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (ht == NULL)
 		return (NULL);
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t*) * size);
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 	return (ht);
 }
