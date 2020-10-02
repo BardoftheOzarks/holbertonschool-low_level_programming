2a7cc57e439990c20d0384a9ca9f102282d854af
BardoftheOzarks <1886@holbertonschool.com>
2020-10-02 23:51:00 +0000

    added NULL test

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,20 +8,19 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned int index = 0, cnt = 0;
 	hash_node_t *scan;
 
+	if (ht == NULL)
+		return;
 	putchar('{');
 	while (index <= ht->size)
 	{
-		if (ht->array[index])
+		scan = ht->array[index];
+		while (scan)
 		{
-			scan = ht->array[index];
-			while (scan)
-			{
-				if (cnt > 0)
-					printf(", ");
-				printf("'%s': '%s'", scan->key, scan->value);
-				cnt++;
-				scan = scan->next;
-			}
+			if (cnt > 0)
+				printf(", ");
+			printf("'%s': '%s'", scan->key, scan->value);
+			cnt++;
+			scan = scan->next;
 		}
 		index++;
 	}
