7a4605d2a30edb00e89cfe0fdfe7f02d2ac753aa
BardoftheOzarks <1886@holbertonschool.com>
2020-10-02 06:00:51 +0000

    added tasks 0 and 1

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of table to be created
+ * Return: pointer to new table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t*) * size);
+	return (ht);
+}
