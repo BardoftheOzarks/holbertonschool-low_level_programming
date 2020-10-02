5e29a9530eaf07e9cc24bfc18a30277070e771ad
BardoftheOzarks <1886@holbertonschool.com>
2020-10-02 23:07:05 +0000

    added list checker

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,15 +9,37 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new = malloc(sizeof(hash_node_t));
+	hash_node_t *scan, *new = malloc(sizeof(hash_node_t));
 
-	if (new == NULL || ht == NULL || key == NULL || strcmp(key, "") == 0)
+	if (new == NULL || ht == NULL || key == NULL || strcmp(key, "") == 0 ||
+		value == NULL)
+	{
+		free(new);
 		return (0);
+	}
 	new->key = strdup(key);
 	new->value = strdup(value);
+	if (new->key == NULL || new->value == NULL)
+	{
+		free(new);
+		return (0);
+	}
 	index = hash_djb2((const unsigned char *)key) % ht->size;
-	if (ht->array[index] != NULL)
-		new->next = ht->array[index];
+	scan = ht->array[index];
+	while (scan != NULL)
+	{
+		if (strcmp(scan->key, key) == 0)
+		{
+			scan->value = strdup(value);
+			free(new->key);
+			free(new->value);
+			free(new);
+			return(1);
+		}
+		else
+			scan = scan->next;
+	}
+	new->next = ht->array[index];
 	ht->array[index] = new;
 	return (1);
 }
