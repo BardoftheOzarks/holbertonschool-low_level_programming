1ba789dd1328b61c76b8ec6c0c50cd69c321bd97
BardoftheOzarks <1886@holbertonschool.com>
2021-01-14 19:54:21 +0000

    added missing []

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,7 +13,7 @@ int linear_search(int *array, size_t size, int value)
 	if (array != NULL)
 		for (i = 0; i < size; i++)
 		{
-			printf("Value checked at array[%d] = %d\n", i, array[i]);
+			printf("Value checked at array[%d] = [%d]\n", i, array[i]);
 			if (array[i] == value)
 				return (i);
 		}
