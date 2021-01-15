737fb4ff12b5f32ab110c6984b7caebf5f133497
BardoftheOzarks <1886@holbertonschool.com>
2021-01-15 05:42:12 +0000

    testing without conditional

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -13,11 +13,12 @@ int interpolation_search(int *array, size_t size, int value)
 	if (array != NULL)
 		while (left <= right)
 		{
-			if (value == array[left])
-			{
-				printf("Value checked array[%d] = [%d]\n", left, array[left]);
-				return (array[left]);
-			}
+			/*if (value == array[left])
+			 *{
+			 *	printf("Value checked array[%d] = [%d]\n", left, array[left]);
+			 *	return (array[left]);
+			 *}
+			 */
 			middle = left + (((double)(right - left) / (array[right] - array[left])) *
 					 (value - array[left]));
 			if (middle > right)
