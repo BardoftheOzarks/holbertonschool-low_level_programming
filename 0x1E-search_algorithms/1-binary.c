3ab9785b574ba7212731e899cf4f6268fa4ca527
BardoftheOzarks <1886@holbertonschool.com>
2021-04-20 17:52:44 +0000

    removed conditional

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -27,7 +27,7 @@ int binary_search(int *array, size_t size, int value)
 				return (middle);
 			else if (array[middle] < value)
 				left = middle + 1;
-			else if (array[middle] > value)
+			else
 				right = middle - 1;
 		}
 	return (-1);
