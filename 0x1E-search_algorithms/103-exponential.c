2ecb08279851f75c71e80f84ddcb88efd536975a
BardoftheOzarks <1886@holbertonschool.com>
2021-04-21 23:07:56 +0000

    fixed spelling

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,35 +1,32 @@
 #include "search_algos.h"
 /**
- * exponential_search - searches a sorted array using the exponential search algorithm
+ * exponential_search - searches using the exponential search algorithm
  * @array: array to be searched
  * @size: size of array
  * @value: value to find
  * Return: index of value, or -1 if not found
  */
 int exponential_search(int *array, size_t size, int value)
-{
-	size_t left, right = 0, middle, i;
+{	size_t left, right = 0, middle, i;
 
 	if (array != NULL)
 	{
 		while (array[right] < value)
-                {
+		{
 			left = right;
 			if (right < 1)
 				right++;
 			else
 				right = right * 2;
 			if (right >= size - 1)
-			{
-				right = size - 1;
-				break;
-			}
-                        printf("Value checked at index [%lu] = [%d]\n", right, array[right]);
-                }
-		printf("Value found between indexes [%lu] and [%lu]\n", left, right);
+			{	right = size - 1;
+				break;			}
+			printf("Value checked at index [%lu] = [%d]\n",
+			       right, array[right]);                }
+		printf("Value found between indexes [%lu] and [%lu]\n",
+		       left, right);
 		while (left <= right)
-		{
-			printf("searching in array: ");
+		{	printf("Searching in array: ");
 			for (i = left; i <= right; i++)
 			{
 				if (i == right)
